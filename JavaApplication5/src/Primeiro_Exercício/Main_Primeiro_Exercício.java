867550f5873aaf8fd2b413283720b7a525ba0d28
Gustavo H. Tomaz Lopes <gustavo.htl@hotmail.com>
2022-02-24 20:40:48 -0300

    Lista de Exercícios questões 1, 2 e 3 pronto

---
+++ JavaApplication5/src/Primeiro_Exercício/Main_Primeiro_Exercício.java
@@ -0,0 +1,15 @@
+package Primeiro_Exercício;
+
+public class Main_Primeiro_Exercício {
+
+    public static void main(String[] args) {
+    
+        
+    double real = 5;
+    
+    double dolar = 5.10;
+    
+        System.out.println("A Conversão de real para dolar é: " + (real / dolar));
+    }
+    
+}
---
+++ JavaApplication5/src/Terceiro_Exercício/Main_Terceiro_Exercício.java
@@ -0,0 +1,16 @@
+package Terceiro_Exercício;
+
+public class Main_Terceiro_Exercício {
+
+    public static void main(String[] args) {
+
+        int variavel = 10;
+
+        if (variavel % 2 == 0) {
+            System.out.println("O Valor é Par");
+        } else {
+            System.out.println("O Valor é Impar");
+        }
+    }
+
+}
