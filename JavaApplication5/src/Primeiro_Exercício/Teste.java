4863070170c84b0fb12a201c393dc3a3260b7795
Gustavo H. Tomaz Lopes <gustavo.htl@hotmail.com>
2022-02-24 10:57:08 -0300

    teste

---
+++ JavaApplication5/src/Primeiro_Exercício/Teste.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primeiro_Exercício;
+
+/**
+ *
+ * @author gusta
+ */
+public class Teste {
+    
+}
